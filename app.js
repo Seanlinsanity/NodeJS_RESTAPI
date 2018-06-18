a9cc015a29a2fddfef84242e7fed7afe1395b829
Sean Lin <seanlin1016@gmail.com>
2018-06-18 15:12:08 +0800

    Connecting to MySQL

--- app.js
+++ app.js
@@ -2,8 +2,39 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
+
 
 app.use(morgan('combined'))
+
+app.get('/user/:id', (req, res) => {
+    console.log(`Fetching user with id: ${req.params.id}`)
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'sean1016',
+        database: 'mysql_pratice'
+    })
+
+    const userId = req.params.id
+    const queryString = 'SELECT * FROM users WHERE id = ?'
+    connection.query(queryString, [userId], (err, rows, fields) =>{
+        if (err){
+            console.log(`Failed to query for users: ${err}`)
+            res.sendStatus(500)
+            return
+        }
+
+        console.log("Fetch mysql users successfully")
+
+        const users = rows.map((row) => {
+            return {firstName: row.first_name, lastName: row.last_name}
+        })
+        res.json(users)
+
+    })
+
+})
 
 app.get('/', (req, res) => {
     console.log('Responding to root route')
